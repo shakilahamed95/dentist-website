3c9d5a5dc73043882db0c583b7999ac2b47bfb24
Your Name <you@example.com>
2022-04-17 12:37:18 +0600

    simple navbar done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.jpg'
+
+const Header = () => {
+
+    return (
+        <div className='mb-5'>
+            <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/"> Dentist Sohel Rana</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#">Services</Nav.Link>
+                            <Nav.Link href="#">Expert</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
